8419451769e9dc3ee6eff19510c9ed64c8c4b548
Brandon <brandonroulstone71@gmail.com>
2024-05-14 16:30:48 +0200

    init

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
+"use client"
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function Home() {
+  const [ data, setData ] = useState('');
+  const fetchdata = async () => {
+    const data  = await axios.get('/api/home');
+    console.log(data);
+  }
+  useEffect(() => {
+    fetchdata()
+  })
+
+  
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2xl text-center text-blue-500">My new portfolio</h1>
+      <h1 className="text-5xl text-center text-blue-500">My new portfolio</h1>
     </div>
   );
 }
